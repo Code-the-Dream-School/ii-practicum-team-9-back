9596aa31d669453d182745cdd28af7bae5ee2a1e
Lily <alcarrillomunoz@gmail.com>
2025-03-29 18:54:41 -0400

    SCRUM-3 backend register and login created

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const favicon = require('express-favicon');
-const logger = require('morgan');
+const cors = require("cors");
+const favicon = require("express-favicon");
+const logger = require("morgan");
 
-const mainRouter = require('./routes/mainRouter.js');
+//user authentication
+const authenticateUser = require("./middleware/authentication");
+
+//routers
+const authRouter = require("./routes/authenticate");
+
+const mainRouter = require("./routes/mainRouter.js");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//middleware to connect to public front end directory - possibly will need to rename with team
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.static('public'))
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // routes
-app.use('/api/v1', mainRouter);
+app.use("/api/v1", mainRouter);
+//new auth route for login and register
+app.use("/auth", authRouter);
+
+//authenticateUser middleware will be used once user logs into account, calling "products" for now, but need router name for when user accesses products/account
+//app.use("/products", authenticateUser, productsRouter);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
+require("dotenv").config();
 const { PORT = 8000 } = process.env;
 const app = require("./app");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+// const listener = () => console.log(`Listening on Port ${PORT}!`);
+// app.listen(PORT, listener);
+
+//connect DB
+const connectDB = require("./db/connect");
+//mongo_URI from .env file
+let mongoURL = process.env.MONGO_URI;
+
+const start = async () => {
+  try {
+    await connectDB(mongoURL);
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
